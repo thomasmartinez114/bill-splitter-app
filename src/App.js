d6df236761b6dae450fd5170fe6673b942b3c9b2
Thomas Martinez <devtommie114@gmail.com>
2025-12-09 00:57:20 -0500

    converted values to numbers

--- src/App.js
+++ src/App.js
@@ -154,21 +154,36 @@ function FormAddFriend({ onAddFriend }) {
 }
 
 function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState('');
+  const [paidByUser, setPaidByUser] = useState('');
+  const [whoIsPaying, setWhoIsPaying] = useState('user');
+
   return (
     <form action='' className='form-split-bill'>
       <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>Bill value</label>
-      <input type='text' />
+      <input
+        type='text'
+        value={bill}
+        onChange={e => setBill(Number(e.target.value))}
+      />
 
       <label>Your expense</label>
-      <input type='text' />
+      <input
+        type='text'
+        value={paidByUser}
+        onChange={e => setPaidByUser(Number(e.target.value))}
+      />
 
       <label>{selectedFriend.name}'s expense</label>
       <input type='text' disabled />
 
       <label>Who is paying the bill?</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={e => setWhoIsPaying(e.target.value)}
+      >
         <option value='user'>You</option>
         <option value='friend'>{selectedFriend.name}</option>
       </select>
